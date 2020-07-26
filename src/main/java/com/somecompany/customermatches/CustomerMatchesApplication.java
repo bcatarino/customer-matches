f53089aba5a19c658584927574c9db3f7f675a4d
Bruno Catarino <bcatarino@gmail.com>
2020-07-26 09:53:59 +0100

    Initial creation of project

---
+++ src/main/java/com/somecompany/customermatches/CustomerMatchesApplication.java
@@ -0,0 +1,13 @@
+package com.somecompany.customermatches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMatchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somecompany/customermatches/CustomerMatchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somecompany.customermatches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMatchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
